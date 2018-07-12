8ca46dacb29cd6063b237a0b5f0128bcd20fe4f0
Ben <btothemose@gmail.com>
2018-07-12 12:40:50 -0500

    folders

---
+++ Python/bmi.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+def gatherinfo():
+    height = float(raw_input("What's your height? (in/m) "))
+    weight = float(raw_input("What's your weight? (lb/kg) "))
+    unit = raw_input("Are you using metric or imperial? ")
+    return(height,weight,unit)
+
+def calculation(height,weight,unit):
+    if unit=="imperial":
+        bmi=703*(weight/(height**2))
+    elif unit=="metric":
+        bmi=weight/(height**2)
+    print("Your BMI is %s" % bmi)
+
+while True:
+    height,weight,unit=gatherinfo()
+    if height<=0 or weight<=0:
+        print("Height and weight must be greater than zero")
+    elif unit.startswith('i'):
+        calculation(height,weight,unit="imperial")
+        break
+    elif unit.startswith('m'):
+        calculation(height,weight,unit="metric")
+        break
+    else:
+        print("Unknown measurement system %s" % unit)
---
+++ Python/timer.py
@@ -0,0 +1,6 @@
+#!/bin/python
+
+import time
+
+start_time = time.localtime()
+print("Timer start at %s" % time.strftime("%X", start_time))
