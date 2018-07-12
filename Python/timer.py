00179803e1c0757d224bd0e5357d4f56e089cdba
Ben <btothemose@gmail.com>
2018-07-12 12:59:03 -0500

    test files removed, timer updated

--- Python/timer.py
+++ Python/timer.py
@@ -3,4 +3,14 @@
 import time
 
 start_time = time.localtime()
+
 print("Timer start at %s" % time.strftime("%X", start_time))
+
+raw_input("Press Enter/Return to continue")
+
+stop_time=time.localtime()
+
+difference=time.mktime(stop_time)-time.mktime(start_time)
+
+print("Timer stopped at %s" % time.strftime("%X", stop_time))
+print("Total time elapsed: %s" % difference)
