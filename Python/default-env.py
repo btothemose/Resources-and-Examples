d31ef2f9d55a4024ca37cef783b83f049b4f4e2a
Ben <btothemose@gmail.com>
2018-07-12 13:18:38 -0500

    updated default-env file

--- Python/default-env.py
+++ Python/default-env.py
@@ -2,7 +2,12 @@
 
 import os
 
+# OS Package used for environment variables
+
 stage=(os.getenv("STAGE") or "development").upper()
+
+# If env variable isn't stated before running script,
+# the script will default to "development" string entry.
 
 output = "We're running in %s" % stage
 
