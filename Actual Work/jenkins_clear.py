d16165e7896e882407c638d50dca39075ecbeee9
Ben <btothemose@gmail.com>
2018-08-30 16:09:02 -0500

    added actual work

--- Actual Work/jenkins_clear.py
+++ Actual Work/jenkins_clear.py
@@ -14,4 +14,11 @@ for file in sort_dir(path):
     full_path_to_file=os.path.join(path,file)
     file_mod_time=os.stat(full_path_to_file).st_mtime
     file_age=(current_time - file_mod_time)/86400
-    
+    if file_mod_time < (current_time - (days_to_keep*86400)):
+        print
+        print ("Deleting %s") % (full_path_to_file)
+        os.remove(full_path_to_file)
+    else:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        break
