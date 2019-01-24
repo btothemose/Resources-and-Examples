daee798e3fb58ac5c61cac858466b10fbb7c74a3
Ben Moseley <ben.moseley@kibocommerce.com>
2019-01-24 15:32:36 -0600

    reverted report back to subprocess example rather than fab attempt

--- Actual Work/report.py
+++ Actual Work/report.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python/
 
-import os
-from fabric.api import *
+import subprocess
+import sys
 
 host="prod-ebcp03.dub.baynote.net"
 command="pwd"
 
-env.user = os.getenv('ben.moseley', 'vagrant')
-env.password = os.getenv('password', 'vagrant')
-
-@hosts(host)
-def do_something():
-    run(command)
+ssh = subprocess.Popen(["ssh", "%s" % host, command],
+                            shell=False,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+result = ssh.stdout.readlines()
+if result == []:
+    error = ssh.stderr.readlines()
+    print >>sys.stderr, "ERROR: %s" % error
+else:
+    print result
