db2e02da11831c86994cb105015f65bd15722a48
Spectre.Linux <btothemose@gmail.com>
2019-01-24 15:30:35 -0600

    attempting to make example fab file

--- Actual Work/report.py
+++ Actual Work/report.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python/
 
-import subprocess
-import sys
+import os
+from fabric.api import *
 
 host="prod-ebcp03.dub.baynote.net"
 command="pwd"
 
-ssh = subprocess.Popen(["ssh", "%s" % host, command],
-                            shell=False,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-result = ssh.stdout.readlines()
-if result == []:
-    error = ssh.stderr.readlines()
-    print >>sys.stderr, "ERROR: %s" % error
-else:
-    print result
+env.user = os.getenv('ben.moseley', 'vagrant')
+env.password = os.getenv('password', 'vagrant')
+
+@hosts(host)
+def do_something():
+    run(command)
